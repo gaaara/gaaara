a5e1b612829d1ad421eed41823ba1b5c551764f5
gaaara <enixdarky2@gmail.com>
2014-03-27 09:59:58 -0400

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -133,8 +133,9 @@ svn co http://svn.rutorrent.org/svn/filemanager/trunk/filemanager
 #######################
 # filemanager  config #       
 #######################
-<?php
+
 cat <<'EOF' >  /var/www/rutorrent/plugins/filemanager/conf.php
+<?php
 $fm['tempdir'] = '/tmp';                // path were to store temporary data ; must be writable
 $fm['mkdperm'] = 755;           // default permission to set to new created directories
 
@@ -156,6 +157,8 @@ $fm['archive']['types'] = array('rar', 'zip', 'tar', 'gzip', 'bzip2');
 $fm['archive']['compress'][0] = range(0, 5);
 $fm['archive']['compress'][1] = array('-0', '-1', '-9');
 $fm['archive']['compress'][2] = $fm['archive']['compress'][3] = $fm['archive']['compress'][4] = array(0);
+
+?>
 EOF
 #######################
 # filemanager  config #       
